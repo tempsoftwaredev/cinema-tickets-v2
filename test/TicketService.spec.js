ddae67cd18ea0e9020024e1fea7591284d658dc0
tempsoftwaredev <alsodavidverweij@gmail.com>
2022-11-03 18:09:15 +0000

    Tests according to ACs

--- test/TicketService.spec.js
+++ test/TicketService.spec.js
@@ -4,7 +4,31 @@ import TicketService from "../src/pairtest/TicketService";
 import { errorSpy } from "./testUtil";
 
 describe("TicketService", () => {
-  const ticketService = new TicketService();
+  const paymentService = jest.fn();
+  const seatService = jest.fn();
+  const pricing = {
+    INFANT: 0,
+    CHILD: 10,
+    ADULT: 20,
+  };
+  const ticketService = new TicketService(paymentService, seatService, pricing);
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should accept multiple tickets of different types", () => {
+    const tickets = [
+      new TicketTypeRequest("ADULT", 1),
+      new TicketTypeRequest("CHILD", 2),
+      new TicketTypeRequest("INFANT", 3),
+    ];
+    const accountId = "1";
+
+    const result = ticketService.purchaseTickets(accountId, ...tickets);
+
+    expect(result).toBe(true);
+  });
 
   it("should not allow more than 20 tickets to be purchased", () => {
     const tickets = new TicketTypeRequest("ADULT", 21);
@@ -12,6 +36,22 @@ describe("TicketService", () => {
 
     const result = new errorSpy(() =>
       ticketService.purchaseTickets(accountId, tickets)
+    ).run();
+
+    expect(result).toBeInstanceOf(InvalidPurchaseException);
+    expect(result.message).toEqual("ticket-service.too-many-tickets");
+  });
+
+  it("should not allow more than 20 tickets to be purchased, across ticket types", () => {
+    const tickets = [
+      new TicketTypeRequest("ADULT", 10),
+      new TicketTypeRequest("CHILD", 10),
+      new TicketTypeRequest("INFANT", 1),
+    ];
+    const accountId = "1";
+
+    const result = new errorSpy(() =>
+      ticketService.purchaseTickets(accountId, ...tickets)
     ).run();
 
     expect(result).toBeInstanceOf(InvalidPurchaseException);
@@ -27,6 +67,75 @@ describe("TicketService", () => {
 
     expect(result).toBeInstanceOf(InvalidPurchaseException);
     expect(result.message).toEqual("ticket-service.no-tickets-provided");
+  });
+
+  it("should not allow child tickets without an adult ticket", () => {
+    const tickets = new TicketTypeRequest("CHILD", 1);
+    const accountId = "1";
+
+    const result = new errorSpy(() =>
+      ticketService.purchaseTickets(accountId, tickets)
+    ).run();
+
+    expect(result).toBeInstanceOf(InvalidPurchaseException);
+    expect(result.message).toEqual("ticket-service.missing-adult-ticket");
+  });
+
+  it("should not allow infant tickets without an adult ticket", () => {
+    const tickets = new TicketTypeRequest("INFANT", 1);
+    const accountId = "1";
+
+    const result = new errorSpy(() =>
+      ticketService.purchaseTickets(accountId, tickets)
+    ).run();
+
+    expect(result).toBeInstanceOf(InvalidPurchaseException);
+    expect(result.message).toEqual("ticket-service.missing-adult-ticket");
+  });
+
+  it("should not allow infant tickets without an adult ticket", () => {
+    const tickets = new TicketTypeRequest("INFANT", 1);
+    const accountId = "1";
+
+    const result = new errorSpy(() =>
+      ticketService.purchaseTickets(accountId, tickets)
+    ).run();
+
+    expect(result).toBeInstanceOf(InvalidPurchaseException);
+    expect(result.message).toEqual("ticket-service.missing-adult-ticket");
+  });
+
+  it("should not reserve seats for infant tickets", () => {
+    const tickets = [
+      new TicketTypeRequest("ADULT", 5),
+      new TicketTypeRequest("CHILD", 6),
+      new TicketTypeRequest("INFANT", 3),
+    ];
+    const accountId = "1";
+
+    const result = ticketService.purchaseTickets(accountId, ...tickets);
+
+    expect(seatService).toHaveBeenCalledWith(Number(accountId), 11);
+  });
+
+  it("should charge the appropriate price for each ticket", () => {
+    const ticketSetup = [["ADULT", 5], ["CHILD", 6], "INFANT", 3];
+    const expectedPrice = ticketSetup.reduce(
+      total,
+      (type, amount) => total + pricing[type] * amount
+    );
+
+    const tickets = ticketSetup.map(
+      ([type, amount]) => new TicketTypeRequest(type, amount)
+    );
+    const accountId = "1";
+
+    const result = ticketService.purchaseTickets(accountId, ...tickets);
+
+    expect(paymentService).toHaveBeenCalledWith(
+      Number(accountId),
+      expectedPrice
+    );
   });
 
   it.each([
